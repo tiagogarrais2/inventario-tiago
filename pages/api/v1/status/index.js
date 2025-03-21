03a42ab4ace702f3fd2107b13f9e2a8749b19f7a
computador <tiagoarraisholanda@gmail.com>
2025-03-21 02:50:33 -0300

    Adicionando banco de dados, configurações, docker, scripts e .

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,7 @@
+import database from "infra/database";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "Está funcionando!" });
+}
