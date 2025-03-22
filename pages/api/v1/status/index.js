a8c71db0bc98a5c3d0d3973c1b87805de23c7406
computador <tiagoarraisholanda@gmail.com>
2025-03-21 23:25:55 -0300

    Limpando o teste de teste

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,5 @@
 import database from "infra/database";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Está funcionando!" });
+  response.status(200).json({});
 }
