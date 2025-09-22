9d8727c1c4c5249760fc6b84b779b2d2be8868fc
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2025-09-22 04:22:44 +0000

    Adicionado um ícone para a aplicação

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,14 +3,17 @@ import "./globals.css";
 export const metadata = {
   title: "Inventário",
   description: "Aplicativo para controle de inventário",
+  icons: {
+    icon: "/favicon-32x32.png", // Ícone padrão
+    shortcut: "/favicon-16x16.png", // Atalho para navegadores antigos
+    apple: "/apple-touch-icon.png", // Ícone para dispositivos Apple
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>
-      {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
