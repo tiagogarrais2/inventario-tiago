6abf501629be878f149e522212775d6d264488bc
Tiago Arrais <tiagoarrais1981@hotmail.com>
2025-09-23 03:31:31 -0300

    Corrigindo erro de prerenderização no cadastro

--- src/app/cadastrar/page.js
+++ src/app/cadastrar/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Cadastrar from "../components/Cadastrar";
 
 export default function CadastrarPage() {
-  return <Cadastrar />;
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <Cadastrar />
+    </Suspense>
+  );
 }
